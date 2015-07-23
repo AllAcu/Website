d6deecdf8d6fbe6cd8a8105800fb27cdd644e0f5
Brett Morien <bmorien@gmail.com>
2015-07-22 18:26:39 -0700

    Add Smtp credentials.

---
+++ Site/Smtp.cs
@@ -0,0 +1,9 @@
+﻿namespace AllAcu
+{
+    public class Smtp
+    {
+        public string Server { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
