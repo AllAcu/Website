ac766606561dc3fb39cba31388e74cfa611b4ccb
Brett Morien <bmorien@gmail.com>
2015-11-29 20:07:41 -0800

    Generate new Sln.

--- Api/DatabaseConnections.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AllAcu
-{
-    public class DatabaseConnections
-    {
-        public string EventStore { get; set; }
-        public string AllAcu { get; set; }
-        public string Authentication { get; set; }
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Website
-The AllAcu website.
-
-[![Build status](https://ci.appveyor.com/api/projects/status/lp6c0kvxt1ei7xat?svg=true)](https://ci.appveyor.com/project/askheaves/website)
