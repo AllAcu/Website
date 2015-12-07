43c308500f7f080aedab72b8564b65031627b7d5
Brett Morien <bmorien@gmail.com>
2015-12-07 11:32:09 -0800

    Add publish profiles. Looks like some lib stuff got shuffled around too.

--- src/Site/wwwroot/_references.js
+++ src/Site/wwwroot/_references.js
@@ -29,5 +29,6 @@
 /// <reference path="lib/hammer.js/hammer.js" />
 /// <reference path="lib/humanize-duration/humanize-duration.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/momentjs/moment.js" />
